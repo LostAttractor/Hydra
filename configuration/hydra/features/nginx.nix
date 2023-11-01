a8f8ded49e9138e49c223df15121d1fe865fbf22
ChaosAttractor <lostattractor@gmail.com>
2023-11-02 05:03:42 +0800

    Hydra/nginx: ACME

--- configuration/hydra/features/nginx.nix
+++ configuration/hydra/features/nginx.nix
@@ -10,10 +10,19 @@
     virtualHosts = {
       "hydra.home.lostattractor.net" = {
         locations."/".proxyPass = config.services.hydra.hydraURL;
+        addSSL = true;
+        enableACME = true;
       };
       "binarycache.home.lostattractor.net" = {
         locations."/".proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
+        addSSL = true;
+        enableACME = true;
       };
     };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "lostattractor@gmail.com";
   };
 }
