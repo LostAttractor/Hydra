50dfe1c703541f893e434b35fc7c4d22b910840b
ChaosAttractor <lostattractor@gmail.com>
2023-11-20 08:14:35 +0800

    NixBuilder: Add `binarycache.home.lostattractor.net` to substituters

--- configuration/nixbuilder/default.nix
+++ configuration/nixbuilder/default.nix
@@ -5,7 +5,9 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" "ca-derivations" ];
 
   # Substituters
-  nix.settings.substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+  nix.settings.substituters = [ "https://binarycache.home.lostattractor.net" "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+
+  nix.settings.trusted-public-keys =  [ "binarycache.home.lostattractor.net:nB258qoytYrdCe2pcI6qJ/M9R0l7Q5l9Bu5ryCbzItc=" ];
 
   # Store Optimise & Auto Clean
   nix.settings.auto-optimise-store = true;
