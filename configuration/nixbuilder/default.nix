c6276d81e475e1290937813e19532d79cb76928d
ChaosAttractor <lostattractor@gmail.com>
2023-12-15 08:07:46 +0800

    hydra & nixbuilder: manage authorizedKeys by nix

--- configuration/nixbuilder/default.nix
+++ configuration/nixbuilder/default.nix
@@ -23,6 +23,13 @@
     max-free = ${toString (30 * 1024 * 1024 * 1024)}
   '';
 
+  users = {
+    # Don't allow mutation of users outside of the config.
+    mutableUsers = false;
+    # Privilege User
+    users.root.openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC5HypvbsI4xvwfd4Uw7D+SV0AevYPS/nCarFwfBwrMHKybbqUJV1cLM1ySZPxXcZD7+3m48Riiwlssh6o7WM/M= openpgp:0xDE4C24F6" ];
+  };
+
   # Basic Packages
   environment.systemPackages = with pkgs; [ htop btop duf ];
 
