2f9150d04aed009286a60180a2d06f35b68fd63a
ChaosAttractor <lostattractor@gmail.com>
2024-06-25 11:01:45 +0800

    features/nix: init and refactor

--- configuration/hydra/default.nix
+++ configuration/hydra/default.nix
@@ -2,10 +2,10 @@
 
 {
   imports = [
+    ../features/nix.nix
     ../features/fish.nix
     ../features/prometheus.nix
     ../features/binfmt.nix
-    ./features/nix.nix
     ./features/builders.nix
     ./features/hydra.nix
     ./features/nix-serve.nix
@@ -21,6 +21,15 @@
     # Privilege User
     users.root.openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC5HypvbsI4xvwfd4Uw7D+SV0AevYPS/nCarFwfBwrMHKybbqUJV1cLM1ySZPxXcZD7+3m48Riiwlssh6o7WM/M= openpgp:0xDE4C24F6" ];
   };
+
+  # Hydra may overrides trusted-users setting
+  nix.settings.trusted-users = [ "root" ];
+
+  # Auto Clean
+  nix.extraOptions = ''
+    min-free = ${toString (30 * 1024 * 1024 * 1024)}
+    max-free = ${toString (60 * 1024 * 1024 * 1024)}
+  '';
 
   # Basic Packages
   environment.systemPackages = with pkgs; [ htop btop duf ];
