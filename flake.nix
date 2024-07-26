e7dd4c37502548b7cad36df9e2e8ff93312a7238
ChaosAttractor <lostattractor@gmail.com>
2024-07-26 16:43:21 +0800

    flake/hydraJobs: add tarball

--- flake.nix
+++ flake.nix
@@ -58,6 +58,9 @@
       nixosConfigurations = mapAttrs' (name: config:
         nameValuePair name config.config.system.build.toplevel)
         nixosConfigurations;
+      tarball = mapAttrs' (name: config:
+        nameValuePair name config.config.system.build.tarball)
+        nixosConfigurations;
     };
   };
 }
