1687b6513b51e66be735bc8b769cf79bd2718189
ChaosAttractor <lostattractor@gmail.com>
2025-01-09 03:30:33 +0800

    hydra/secrets/nix: fixup permissions

--- configuration/hydra/secrets/nix/access-tokens.nix
+++ configuration/hydra/secrets/nix/access-tokens.nix
@@ -8,7 +8,8 @@
     content = ''
       access-tokens = github.com=${config.sops.placeholder."nix/access-tokens/github"}
     '';
-    group = "wheel";
+    # 目前我还不使用 wheel 组, 不过如果这会导致问题的话, 或许也可以给 hydra user 加到 wheel 组里?
+    group = "hydra";
     mode = "0440";
   };
 
