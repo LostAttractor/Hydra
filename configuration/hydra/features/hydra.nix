5e5235f066eeab125bfdff52ca44a430a3b12f70
ChaosAttractor <lostattractor@gmail.com>
2025-01-09 03:31:02 +0800

    hydra: email & metrics

--- configuration/hydra/features/hydra.nix
+++ configuration/hydra/features/hydra.nix
@@ -7,17 +7,36 @@
   # Hydra
   services.hydra = {
     enable = true;
-    hydraURL = "https://hydra.home.lostattractor.net";
-    notificationSender = "hydra@lostattractor.net";
+    hydraURL = "https://hydra.lostattractor.net";
     useSubstitutes = true;
+    notificationSender = "Hydra <root@lostattractor.net>";
+    smtpHost = "smtp.gmail.com";
     extraConfig = ''
+      email_notification = 1
       max_output_size = 34359738367 # 1024^3 * 32 - 1
+
+      queue_runner_metrics_address = [::]:9198
+      <hydra_notify>
+        <prometheus>
+          listen_address = 0.0.0.0
+          port = 9199
+        </prometheus>
+      </hydra_notify>
     '';
   };
 
-  services.nginx.virtualHosts."hydra.home.lostattractor.net" = {
+  networking.firewall.allowedTCPPorts = [ 9198 9199 ];
+
+  systemd.services.hydra-notify = {
+    serviceConfig.EnvironmentFile = config.sops.secrets."email".path;
+  };
+
+  services.nginx.virtualHosts."hydra.lostattractor.net" = {
     locations."/".proxyPass = "http://localhost:${toString config.services.hydra.port}";
     forceSSL = true;
     enableACME = true;
+    serverAliases = [ "hydra.home.lostattractor.net" ];
   };
+
+  sops.secrets."email" = {};
 }
